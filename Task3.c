4dec4b27777d258e7c5a42a5f722c1fc7736f8db
balabanskiy82-del <balabanskiy82@icloud.com>
2026-09-20 19:49:58 +0300

    Implement meter to kilometer conversion with tests
    Added assertions to test kilometer conversion from meters.

--- Task3.c
+++ Task3.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <assert.h>
 
+int main(void) {
+    assert(1500 / 1000 == 1);
+    assert(2700 / 1000 == 2);
+    assert(14300 / 1000 == 14);
+    printf("все тесты пройдены\n");
+
+    
+    
+    long long meters;
+    scanf("%lld", &meters);
+    long long km = meters / 1000;
+    printf("%lld\n", km);
+
+    return 0;
+}
